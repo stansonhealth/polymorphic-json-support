65bda5675b1bfba2d7f468e2fbed507359fd62ba
Andrew Hall <andrew_hall@premierinc.com>
2022-01-24 15:30:45 -0800

    Prepare to open source library

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import java.net.URI
 plugins {
     `java-library`
     `maven-publish`
+    signing
     groovy
 }
 
@@ -16,53 +17,106 @@ group = "com.stansonhealth"
 version = "1.0-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 
+val publicationName = "polymorphicJsonSupport"
+val isReleaseVersion: Boolean = !(version as String).endsWith("SNAPSHOT")
+val ossrhUsername: String = findProperty("sonatypeUsername") as String
+val ossrhPassword: String = findProperty("sonatypePassword") as String
+val signingKey: String = findProperty("signingKey") as String
+val signingPassword: String = findProperty("signingPassword") as String
+
 dependencies {
     compileOnly("org.hibernate:hibernate-core:5.6.3.Final")
     api("com.vladmihalcea:hibernate-types-52:2.14.0")
 
-    testImplementation("org.codehaus.groovy:groovy:3.0.7")
-    testImplementation("org.spockframework:spock-core:2.0-M4-groovy-3.0")
-    testImplementation("junit:junit:4.13.1")
+    testImplementation("org.codehaus.groovy:groovy:3.0.8")
+    testImplementation("org.spockframework:spock-core:2.0-groovy-3.0")
+    testImplementation("junit:junit:4.13.2")
     testImplementation("org.hibernate:hibernate-core:5.6.3.Final")
-}
-
-val sourcesJar by tasks.registering(Jar::class) {
-    archiveClassifier.set("sources")
-    from(sourceSets.main.get().allSource)
 }
 
 tasks {
 
-    artifacts {
-        archives(sourcesJar)
-        archives(jar)
+    val sourcesJar by registering(Jar::class) {
+        archiveClassifier.set("sources")
+        from(sourceSets.main.get().allSource)
     }
 
+    val javadocJar by registering(Jar::class) {
+        dependsOn.add(javadoc)
+        archiveClassifier.set("javadoc")
+        from(javadoc)
+    }
+
+    jar {
+        dependsOn.addAll(listOf(sourcesJar, javadocJar))
+        duplicatesStrategy = DuplicatesStrategy.INCLUDE
+    }
+
+    javadoc {
+        setDestinationDir(File("${rootDir}/docs"))
+    }
+
+    test {
+        useJUnitPlatform()
+    }
+
+    artifacts {
+        archives(sourcesJar)
+        archives(javadocJar)
+        archives(jar)
+    }
+}
+
+java {
+    withJavadocJar()
+    withSourcesJar()
 }
 
 publishing {
-    val stansonMavenRepo: String by System.getProperties()
-    val awsUser: String by System.getProperties()
-    val mavenPassword: String by System.getProperties()
     repositories {
         maven {
-            name = "ecr"
-            url = URI(stansonMavenRepo)
+            val releaseRepo = "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
+            val snapshotRepo = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
+            name = "OSSRH"
+            url = URI(if (isReleaseVersion) releaseRepo else snapshotRepo)
             credentials {
-                username = awsUser
-                password = mavenPassword
+                username = ossrhUsername
+                password = ossrhPassword
             }
         }
     }
     publications {
-        register("springProtect", MavenPublication::class) {
+        register(publicationName, MavenPublication::class) {
             from(components["java"])
-            artifact(sourcesJar.get())
+            pom {
+                groupId = project.group as String?
+                name.set(project.name)
+                description.set("Add jackson based polymorphic support to JSON Hibernate Type")
+                url.set("https://github.com/stansonhealth/polymorphic-json-support")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("andrewhall")
+                        name.set("Andrew Hall")
+                        email.set("andrew_hall@premierinc.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git@github.com:stansonhealth/polymorphic-json-support.git")
+                    developerConnection.set("scm:git:git@github.com:stansonhealth/polymorphic-json-supportgit")
+                    url.set("https://github.com/stansonhealth/polymorphic-json-support")
+                }
+            }
         }
     }
 }
 
-
-tasks.test {
-    useJUnitPlatform()
+signing {
+    useInMemoryPgpKeys(signingKey, signingPassword)
+    sign(publishing.publications.getByName(publicationName))
 }
