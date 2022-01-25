0d11fc54ce2334804100905ccd63af88ff32e01c
Andrew Hall <andrew_hall@premierinc.com>
2022-01-25 08:58:42 -0800

    Add copyright headers

--- src/main/java/com/stansonhealth/polymorphic/PolymorphicJsonTypeSerializerSupplier.java
+++ src/main/java/com/stansonhealth/polymorphic/PolymorphicJsonTypeSerializerSupplier.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2022
+ * Premier, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.stansonhealth.polymorphic;
 
 import static com.stansonhealth.polymorphic.PolymorphicJsonTypeSupport.*;
@@ -8,12 +24,6 @@ import com.vladmihalcea.hibernate.type.util.JsonSerializerSupplier;
 import com.vladmihalcea.hibernate.type.util.ObjectMapperWrapper;
 
 public class PolymorphicJsonTypeSerializerSupplier implements JsonSerializerSupplier {
-
-    private final PolymorphicSerializer polymorphicSerializer;
-
-    public PolymorphicJsonTypeSerializerSupplier() {
-        this.polymorphicSerializer = buildPolymorphicSerializer();
-    }
 
     static class PolymorphicSerializer implements JsonSerializer {
         private final ObjectMapperWrapper objectMapperWrapper;
@@ -30,6 +40,11 @@ public class PolymorphicJsonTypeSerializerSupplier implements JsonSerializerSupp
                     Object.class
             );
         }
+    }
+    private final PolymorphicSerializer polymorphicSerializer;
+
+    public PolymorphicJsonTypeSerializerSupplier() {
+        this.polymorphicSerializer = buildPolymorphicSerializer();
     }
 
     @Override
