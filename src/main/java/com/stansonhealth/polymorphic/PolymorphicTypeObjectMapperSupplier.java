a3c89d25e4dff52b12ad039ab09efb9c09ed3d12
Andrew Hall <andrew_hall@premierinc.com>
2022-01-25 08:59:50 -0800

    Add space

--- src/main/java/com/stansonhealth/polymorphic/PolymorphicTypeObjectMapperSupplier.java
+++ src/main/java/com/stansonhealth/polymorphic/PolymorphicTypeObjectMapperSupplier.java
@@ -14,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.stansonhealth.polymorphic;
 
 import static com.stansonhealth.polymorphic.PolymorphicJsonTypeSupport.*;
